7b5352983d1a80fcde83b25a8a2a25317cea01d1
Italo Siqueira <siqueira.italo@gmail.com>
2018-09-21 17:34:11 -0400

    Initial add

--- HttpClientTest/Program.cs
+++ HttpClientTest/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace HttpClientTest
 {
@@ -6,7 +9,20 @@ namespace HttpClientTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var dta = new { name = "USER 1", number = "123456789", checksum = "0" };
+
+                using (var queryString = new StringContent(JsonConvert.SerializeObject(dta), Encoding.UTF8, "application/json"))
+                {
+                    var response = client.PostAsync("", queryString).Result;
+                    response.EnsureSuccessStatusCode();
+                    var conteudo = response.Content.ReadAsStringAsync().Result;
+                }
+            }
         }
     }
 }
